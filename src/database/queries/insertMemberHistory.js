d0e1c23d56e9f745781e7155806abdf57c533d02
sultanassi95 <mrsultanassi@gmail.com>
2018-01-13 15:45:23 +0200

    added deleting member confirmation, fixed null values for date in members_history

--- src/database/queries/insertMemberHistory.js
+++ src/database/queries/insertMemberHistory.js
@@ -1,9 +1,11 @@
 const dbConnection = require('../dbConnection');
 
 const insertMemberHistory = (memberRow, cb) => {
+  const dateObj = new Date();
+  const dateNow = `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`;
   const historySql = {
-    text: 'insert into members_history (member_id, cwb, cwa, fccb, fcca, notes) values ($1, $2, $3, $4, $5, $6)',
-    values: [memberRow.id, memberRow.cwb, memberRow.cwa, memberRow.fccb, memberRow.fcca, memberRow.notes],
+    text: 'insert into members_history (member_id, cwb, cwa, fccb, fcca, notes, date) values ($1, $2, $3, $4, $5, $6, $7)',
+    values: [memberRow.id, memberRow.cwb, memberRow.cwa, memberRow.fccb, memberRow.fcca, memberRow.notes, dateNow],
   };
   dbConnection.query(historySql, (dataBaseConnectionError, newMemberHistory) => {
     if (dataBaseConnectionError) return cb(dataBaseConnectionError);
